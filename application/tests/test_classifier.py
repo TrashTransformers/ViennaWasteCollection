5b38574d8052ef138f3a7339c85ae4fb3dfedb39
Tamas Molnar <tamas.molnar@nagarro.com>
2023-03-23 16:45:47 +0100

    fix test

--- application/tests/test_classifier.py
+++ application/tests/test_classifier.py
@@ -2,9 +2,5 @@ from trash_ai import classify_image
 
 
 def test_classification_plastic():
-    result = classify_image(
-        "https://www.ikea.com/at/de/images/products/"
-        + "spartansk-wasserflasche-klarglas-gruen__1062060_pe850637_s5"
-        + ".jpg?f=xl"
-    )
-    assert result.category == "plastic"
+    result = classify_image("../images/paper/paper7.jpg")
+    assert result.category == "paper"
