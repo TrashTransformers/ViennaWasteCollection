f4009031c0aaa447341e62d7bd1464855286c35c
Tamas Molnar <tamas.molnar@nagarro.com>
2023-03-24 09:20:40 +0100

    remo test

--- application/tests/test_classifier.py
+++ application/tests/test_classifier.py
@@ -1,13 +1,6 @@
-from classifiers.azure_computer_vision import demo_call
 from trash_ai import classify_image
 
 
 def test_classification_plastic():
     result = classify_image("../images/paper/paper7.jpg")
     assert result.category == "paper"
-
-
-def test_azure_computer_vision():
-    result = demo_call("../images/paper/paper10.jpg")
-    print(result)
-    assert len(result) > 0
