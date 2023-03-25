b0f977eb4fdbdacfad10bbeed7c4bd77562e31cb
Dejan Linkic <dejan.linkic@nagarro.com>
2023-03-25 13:08:27 +0100

    cleanup java
    Signed-off-by: Dejan Linkic <dejan.linkic@nagarro.com>

--- java/src/test/java/org/waste/locator/integration/WasteRecyclingIntegrationTest.java
+++ java/src/test/java/org/waste/locator/integration/WasteRecyclingIntegrationTest.java
@@ -1,14 +1,20 @@
 package org.waste.locator.integration;
 
-import org.junit.jupiter.api.Test;
-import org.waste.locator.entity.CollectionPoint;
-import org.waste.locator.service.WasteTypeService;
-
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.waste.locator.entity.CollectionPoint;
+import org.waste.locator.service.WasteTypeService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class WasteRecyclingIntegrationTest {
-    private final int port = 8081;
+	@LocalServerPort
+    private int port;
     private final String coordinates = "57.548142,68.154844";
 
     @Test
@@ -51,7 +57,7 @@ public class WasteRecyclingIntegrationTest {
 
     @Test
     public void shouldReturnMetallCollectionPoint() {
-        String result = callService("metall", coordinates);
+        String result = callService("metal", coordinates);
 
         assertThat("Gelbe Tonne - Adresse: Telephonweg neben 412; 22. Bezirk").isEqualTo(result);
     }
